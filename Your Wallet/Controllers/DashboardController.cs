dd4fe0c69a9910591df54b332141e123c416d30e
osama <osama392maher@gmail.com>
2024-05-17 19:14:29 +0300

    Expenses Catogries Chart

--- Your Wallet/Controllers/DashboardController.cs
+++ Your Wallet/Controllers/DashboardController.cs
@@ -1,7 +1,10 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Globalization;
 using Your_Wallet.Areas.Identity.Data;
+using Your_Wallet.Models;
 using Your_Wallet.Models.Data;
 
 namespace Your_Wallet.Controllers
@@ -21,7 +24,7 @@ namespace Your_Wallet.Controllers
 
         public async Task<IActionResult> IndexAsync()
         {
-
+            // Redirect to additional info page if user has not set their name
             var user = await _userManager.GetUserAsync(User);
 
             if (!user.NameSet)
@@ -29,10 +32,51 @@ namespace Your_Wallet.Controllers
                 return RedirectToAction("AdditionalInfo", "Account");
             }
 
+            List<Transaction> Transactions = await _context.Transactions
+                                .Include(x => x.Category)
+                                .Where(x => x.ApplicationUserId == user.Id)
+                                .ToListAsync();
 
-            //Total Income
 
-            ViewBag.pic = user.ProfilePicture;
+            // Total Income
+            decimal totalIncome = Transactions
+                .Where(x => x.Category.Type == CategoryType.Income)
+                .Sum(x => x.Amount);
+
+            ViewBag.TotalIncome = totalIncome.ToString("C0");
+
+            // Total Expense
+            decimal totalExpense = Transactions
+                .Where(x => x.Category.Type == CategoryType.Expense)
+                .Sum(x => x.Amount);
+
+            ViewBag.TotalExpense = totalExpense.ToString("C0");
+
+
+            // Balance
+            decimal Balance = (totalIncome + user.StartingBalance ?? 0) - totalExpense;
+
+            CultureInfo culture = CultureInfo.CreateSpecificCulture("en-US");
+            culture.NumberFormat.CurrencyNegativePattern = 1;
+            ViewBag.Balance = String.Format(culture, "{0:C0}", Balance);
+
+
+            var Expenses = Transactions
+                .Where(x => x.Category.Type == CategoryType.Expense)
+                .GroupBy(j => j.Category.Id)
+                .Select(k => new
+                {
+                    title = k.FirstOrDefault().Category.TitleDisplay,
+                    totalAmmount = k.Sum(x => x.Amount),
+                    AmountToDisplay = k.Sum(j => j.Amount).ToString("C0")
+                })
+                .OrderByDescending(l => l.totalAmmount)
+                .ToList();
+
+            ViewBag.Expenses = Expenses;
+
+
+
             return View();
         }
     }
